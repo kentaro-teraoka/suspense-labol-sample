1b6f2d3b531f69afb30acc2eb8b23c7cd4f37a70
k-teraoka <k-teraoka@labol.co.jp>
2025-07-25 18:29:18 +0900

    演算子修正

--- app/pages/mypage/invoice/confirm.vue
+++ app/pages/mypage/invoice/confirm.vue
@@ -5,25 +5,25 @@
     <div class="space-y-4">
       <dl>
         <dt class="font-bold mb-2">請求書</dt>
-        <dd class="pb-4 mb-4 border-b-1 border-stone-300">{{ invoiceForm.invoice ?? "-" }}</dd>
+        <dd class="pb-4 mb-4 border-b-1 border-stone-300">{{ invoiceForm.invoice || "-" }}</dd>
 
         <dt class="font-bold mb-2">買取希望額</dt>
-        <dd class="pb-4 mb-4 border-b-1 border-stone-300">{{ invoiceForm.amount ?? "-" }}</dd>
+        <dd class="pb-4 mb-4 border-b-1 border-stone-300">{{ invoiceForm.amount || "-" }}</dd>
 
         <dt class="font-bold mb-2">取引先会社</dt>
-        <dd class="pb-4 mb-4 border-b-1 border-stone-300">{{ invoiceForm.company ?? "-" }}</dd>
+        <dd class="pb-4 mb-4 border-b-1 border-stone-300">{{ invoiceForm.company || "-" }}</dd>
 
         <dt class="font-bold mb-2">担当者名</dt>
-        <dd class="pb-4 mb-4 border-b-1 border-stone-300">{{ invoiceForm.person ?? "-" }}</dd>
+        <dd class="pb-4 mb-4 border-b-1 border-stone-300">{{ invoiceForm.person || "-" }}</dd>
 
         <dt class="font-bold mb-2">電話番号</dt>
-        <dd class="pb-4 mb-4 border-b-1 border-stone-300">{{ invoiceForm.phone ?? "-" }}</dd>
+        <dd class="pb-4 mb-4 border-b-1 border-stone-300">{{ invoiceForm.phone || "-" }}</dd>
 
         <dt class="font-bold mb-2">ご利用目的</dt>
-        <dd class="pb-4 mb-4 border-b-1 border-stone-300">{{ invoiceForm.purpose ?? "-" }}</dd>
+        <dd class="pb-4 mb-4 border-b-1 border-stone-300">{{ invoiceForm.purpose || "-" }}</dd>
 
         <dt class="font-bold mb-2">エビデンス</dt>
-        <dd class="pb-4 mb-4 border-b-1 border-stone-300">{{ invoiceForm.evidence ?? "-" }}</dd>
+        <dd class="pb-4 mb-4 border-b-1 border-stone-300">{{ invoiceForm.evidence || "-" }}</dd>
       </dl>
     </div>
   </div>
