59826f62c0f447cce09f8108d1e11053be46734d
k-teraoka <k-teraoka@labol.co.jp>
2025-07-30 16:29:00 +0900

    options apiを用意

---
+++ app/components/TestButtons.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex gap-2">
+    <button class="min-w-20 p-2 rounded-md bg-primary-500" @click="$emit('increment')">{{ incrementBtnText }}</button>
+    <button class="min-w-20 p-2 rounded-md bg-primary-500" @click="$emit('decrement')">{{ decrementBtnText }}</button>
+    <button class="min-w-20 p-2 rounded-md bg-primary-500" @click="$emit('reset')">{{ resetBtnText }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    incrementBtnText: {
+      type: String,
+      default: "Increment"
+    },
+    decrementBtnText: {
+      type: String,
+      default: "Decrement"
+    },
+    resetBtnText: {
+      type: String,
+      default: "Reset",
+    },
+  },
+  emits: ["increment", "decrement", "reset"],
+}
+</script>
---
+++ app/pages/test.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="space-y-4">
+    <div>
+      <p>message: {{ message }}</p>
+      <p>count: {{ count }}</p>
+      <p>doubleCount: {{ doubleCount }}</p>
+    </div>
+  
+    <TestButtons
+      incrementBtnText="+"
+      decrementBtnText="-"
+      resetBtnText="リセット"
+      @increment="increment"
+      @decrement="decrement"
+      @reset="reset"
+    />
+  </div>
+</template>
+
+<script>
+import TestButtons from "~/components/TestButtons.vue";
+
+export default {
+  components: {
+    TestButtons,
+  },
+  data() {
+    return {
+      message: "",
+      count: 0,
+    };
+  },
+  computed: {
+    doubleCount() {
+      return this.count * 2;
+    }
+  },
+  methods: {
+    increment() {
+      this.count++;
+      this.message = "増えました";
+    },
+    decrement() {
+      this.count--;
+      this.message = "減りました";
+    },
+    reset() {
+      this.count = 0;
+      this.message = "リセットしました";
+    }
+  }
+};
+</script>
