7d2e9c1ec065f6b0d924d6f4500b2509f9430ddc
k-teraoka <k-teraoka@labol.co.jp>
2025-07-30 16:38:49 +0900

    script -> script setup

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -17,64 +17,23 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import TestButtons from "~/components/TestButtons.vue";
 
-export default {
-  setup() {
-    const message = ref("")
-    const count = ref(0)
-    const doubleCount = computed(() => count.value * 2);
+const message = ref("")
+const count = ref(0)
+const doubleCount = computed(() => count.value * 2);
 
-    const increment = () => {
-      count.value++;
-      message.value = "増えました";
-    };
-    const decrement = () => {
-      count.value--;
-      message.value = "減りました";
-    };
-    const reset = () => {
-      count.value = 0;
-      message.value = "リセットしました";
-    };
-
-    return {
-      message,
-      count,
-      doubleCount,
-      increment,
-      decrement,
-      reset,
-    };
-  },
-  components: {
-    TestButtons,
-  },
-  // data() {
-  //   return {
-  //     message: "",
-  //     count: 0,
-  //   };
-  // },
-  // computed: {
-  //   doubleCount() {
-  //     return this.count * 2;
-  //   }
-  // },
-  // methods: {
-  //   increment() {
-  //     this.count++;
-  //     this.message = "増えました";
-  //   },
-  //   decrement() {
-  //     this.count--;
-  //     this.message = "減りました";
-  //   },
-  //   reset() {
-  //     this.count = 0;
-  //     this.message = "リセットしました";
-  //   }
-  // }
+const increment = () => {
+  count.value++;
+  message.value = "増えました";
+};
+const decrement = () => {
+  count.value--;
+  message.value = "減りました";
+};
+const reset = () => {
+  count.value = 0;
+  message.value = "リセットしました";
 };
 </script>
